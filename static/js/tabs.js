bed47901cc763395cf176190a2c1b9d6bc1702cb
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2022-05-30 00:17:10 +0000

    Updated tabs.

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -1,5 +1,16 @@
 $(function() {
 
+    $('.tabs-list').on('click', '.tab', function(e) {
+        // En este caso, evita que se abran los links cuando se clique en ellos
+        e.preventDefault();
     
-
+        $('.tab').removeClass('active');
+        $('.tab-content').removeClass('show');
+    
+        $(this).addClass('active');
+        // Recoge el valor del href que es igual al id del contenido, y le añade la clase "show"
+        $($(this).attr('href')).addClass('show');
+    
+    });
+    
 });
