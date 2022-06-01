58f9d18767cf26cc5d4a4892fff3a2a49873ba61
xio28 <xiomara281190@gmail.com>
2022-06-01 19:10:52 +0100

    Updated DATABASE.sql 2.1.9.

--- DATABASE.sql
+++ DATABASE.sql
@@ -2,64 +2,71 @@ DROP DATABASE IF EXISTS le_bakery;
 CREATE DATABASE le_bakery;
 USE le_bakery;
 
-CREATE TABLE users (
+CREATE TABLE clients (    
     users_id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50),
     email VARCHAR(50) UNIQUE,
-    pass VARCHAR(50 NOT NULL
-);
-
-CREATE TABLE clients (
+    pass VARCHAR(50 NOT NULL,
     nid VARCHAR(9) UNIQUE,
     c_name VARCHAR(80) NOT NULL,
-    client_id INT AUTO_INCREMENT, -- Quitarlo si no hace falta.
     surname1 VARCHAR(80) NOT NULL,
     surname2 VARCHAR(80) NULL,
     street VARCHAR(100) NOT NULL,
     s_number INT NOT NULL,
+    story INT, -- Story es el piso
     postal_code INT NOT NULL,
     city VARCHAR(50) NOT NULL,
-    contact VARCHAR(12),
-    PRIMARY KEY (nid, client_id), -- en clientes, el nid y el username son clave primaria compuesta ¿?
-    FOREIGN KEY (client_id) REFERENCES users(users_id)
+    contact VARCHAR(12)
 );
 
-CREATE TABLE employees (
-    emp_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE employees (    
+    users_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    pass VARCHAR(50 NOT NULL,
     e_name VARCHAR(80) NOT NULL,
     surname1 VARCHAR(80) NOT NULL,
     surname2 VARCHAR(80) NULL,
-    admin BOOLEAN DEFAULT false,
-    FOREIGN KEY (emp_id) REFERENCES users(users_id)
+    admin_user BOOLEAN DEFAULT false
 );
 
 CREATE TABLE products (
     product_id INT PRIMARY KEY AUTO_INCREMENT,
     p_name VARCHAR(80) NOT NULL,
-    price DEC(5,2) NOT NULL,
+    cost_price DEC(5,2) NOT NULL,
     category VARCHAR(50) NOT NULL,
-    stock INT DEFAULT 0
+    stock INT DEFAULT 0,
+    producto_img VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
-    client_id INT,
     discount DEC(5,2) NOT NULL,
-    subtotal DEC(5,2) NOT NULL,
-    total DEC(5,2) NOT NULL,
-    order_date DATETIME NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients(client_id)
+    total DEC(5,2) NOT NULL
 );
 
 CREATE TABLE invoices (
-    inv_id INT NOT NULL,
-    client_id INT NOT NULL,
-    product_id INT NOT NULL,
+    inv_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT NOT NULL,
-    PRIMARY KEY (inv_id, product_id, client_id, order_id),
-    FOREIGN KEY (client_id) REFERENCES clients(client_id),
+    p_name VARCHAR(80) NOT NULL,
+    p_units INT NOT NULL,
+    retail_price DEC(5,2) NOT NULL,
+    nid VARCHAR(9) UNIQUE,
+    c_name VARCHAR(80) NOT NULL,
+    subtotal DEC(5,2) NOT NULL,
+    total DEC(5,2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+CREATE TABLE employees_orders_clients (
+    emp_id INT NOT NULL,
+    client_id INT NOT NULL,
+    order_id INT NOT NULL,
+    order_date DATETIME,
+    PRIMARY KEY (id_order, emp_id, client_id),
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    FOREIGN KEY (emp_id) REFERENCES employees(emp_id),
+    FOREIGN KEY (client_id) REFERENCES clients(client_id)
 );
 
 CREATE TABLE employees_orders (
@@ -67,14 +74,16 @@ CREATE TABLE employees_orders (
     order_id INT NOT NULL,
     order_prepared DATETIME,
     PRIMARY KEY (id_order, emp_id),
-    FOREIGN KEY (emp_id) REFERENCES employees(emp_id),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (emp_id) REFERENCES employees(emp_id)
 );
 
 CREATE TABLE products_orders (
     order_id INT NOT NULL,
     product_id INT NOT NULL,
-    amount INT NOT NULL,
+    units INT NOT NULL,
+    retail_price DEC(5,2) NOT NULL,
+    subtotal DEC(5,2) NOT NULL,
     PRIMARY KEY (product_id, order_id),
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
@@ -84,8 +93,7 @@ CREATE TABLE clients_products (
     client_id VARCHAR(9) NOT NULL,
     product_id INT NOT NULL,
     rating INT NOT NULL,
-    comment VARCHAR(100),
-    PRIMARY KEY (product_id, client_nid),
+    PRIMARY KEY (product_id, client_id),
     FOREIGN KEY (client_id) REFERENCES clients(client_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
