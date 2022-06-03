c03c4be77704d070e57b46e299496c669d3c5ceb
xio28 <xiomara281190@gmail.com>
2022-06-03 20:26:55 +0100

    Added static routes to main and modified registration template and register.py.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import os
+import sys
+sys.path.append('model') # add the models directory to the path
+sys.path.append('controller')
+# sys.path.append('view')
+
 import sqlite3
-from bottle import *
+from bottle import route, run, template, request, get, post, redirect, static_file, error, debug
+from controller.register import RegistrationForm
+
+@get('/registration')
+def register():
+    form = RegistrationForm(request.POST)
+    return template('registration', form=form)
 
 @get('/')
 def index():
-    pass
+    return template('index')
 
 @get('/users/clients')
 def clients():
@@ -24,6 +36,28 @@ def products():
 @get('/order')
 def order():
     pass
+
+
+# Static Routes
+@get("/static/styles/<filepath:re:.*\.css>")
+def css(filepath):
+    return static_file(filepath, root="static/styles")
+
+@get("/static/fonts/<filepath:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
+def font(filepath):
+    return static_file(filepath, root="static/fonts")
+
+@get("/static/resources/img/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
+def img(filepath):
+    return static_file(filepath, root="static/resources/img")
+
+@get("/static/resources/video/<filepath:re:.*\.mp4>")
+def video(filepath):
+    return static_file(filepath, root="static/resources/video")
+
+@get("/static/js/<filepath:re:.*\.js>")
+def js(filepath):
+    return static_file(filepath, root="static/js")
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True, reloader=True)
