0636d2398c0a52c90a22e45921c1294e8d481523
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2022-06-12 10:30:23 +0000

    Updated admin panel.

--- controller/login.py
+++ controller/login.py
@@ -2,14 +2,14 @@ from wtforms import Form, PasswordField, SubmitField, EmailField, validators
 
 class ContactForm(Form):
 
-    email = EmailField('Email', [
+    email = EmailField([
                                         validators.InputRequired(message="Campo vacío."),
                                         validators.Email(message="Email incorrecto.")
                                     ],
-                                    render_kw={'class': 'g-row-2 g-col-3'})
+                                    render_kw={'class': '', 'placeholder': 'Email'})
     password = PasswordField([
                                         validators.Length(min=10, max=60),
                                         validators.InputRequired()
                                     ], 
-                                        render_kw={'placeholder': 'Contraseña'})
+                                        render_kw={'class': '', 'placeholder': 'Contraseña'})
     save = SubmitField('Log In')
