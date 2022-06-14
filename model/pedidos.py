954e7b84eb37b4333e1bb79d0301822284d843b4
Jerobel Rodriguez <90897331+jerorguez@users.noreply.github.com>
2022-06-14 13:03:07 +0100

    Recovery

--- model/pedidos.py
+++ model/pedidos.py
@@ -1,4 +1,30 @@
+from model.carrito import Carrito
 from model.tablas import *
+from model.modules import *
+from model.compras import *
 
 class Pedidos(Tablas):
-    pass
+    @classmethod
+    def gen_order_id(cls):
+        ref = Compras.field_select[['MAX(ID)']]
+        return 1 if ref == None else 2
+    
+    @classmethod
+    def do_order(cls, id_client):
+        values = Carrito.get_select(['*'], {'IdCliente':id_client})[0]
+        query = f"INSERT INTO pedidos (IdProducto, IdCliente, Unidades) VALUES {values}"
+
+        try:
+            conn = cls._connect()
+            c = conn.cursor()
+            c.execute(query)
+            conn.commit()
+            c.close()
+
+        
+        except sqlite3.Error as error:
+            print('Error while executing sqlite script', error)
+
+        finally:
+            if conn:
+                conn.close()
